4339fa045493839e78b1f73dbb4caa4bb1d56de8
hem0307 <hemkhatri0307@gmail.com>
2026-04-13 10:38:18 +0530

    Add files via upload

---
+++ Unit_4_Middleware.js
@@ -0,0 +1,22 @@
+var exp=require('express')
+var app=exp()
+
+const addName=(req,res,next)=>{
+    req.name="abc"
+    console.log("Name added")
+    next()
+}
+const addcol=(req,res,next)=>{
+    req.col="LJU"
+    console.log("College added")
+    next()
+}
+const addMarks=(req,res,next)=>{
+    req.total=50+40
+    console.log("Marks added")
+    next()
+}
+app.get("/student",addName,addcol,addMarks,(req,res)=>{
+    res.send("student Name: "+req.name+" student College: "+req.col+" Total Marks: "+req.total)
+})
+app.listen(8082,console.log("http://localhost:8082/student"))
